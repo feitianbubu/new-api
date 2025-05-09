1b369237ebd6b482c44a8e44e1263c758d16cbf1
skynono <6811626@qq.com>
2025-05-09 14:31:07 +0800

    feat: log user msg info

--- common/constants.go
+++ common/constants.go
@@ -69,6 +69,7 @@ var EmailLoginAuthServerList = []string{
 
 var DebugEnabled bool
 var MemoryCacheEnabled bool
+var ResponseLogEnabled bool
 
 var LogConsumeEnabled = true
 
--- web/src/components/LogsTable.js
+++ web/src/components/LogsTable.js
@@ -1013,6 +1013,20 @@ const LogsTable = () => {
           key: t('计费过程'),
           value: content,
         });
+        let msgInput = other?.msg_info?.msg_input;
+        let msgOutput = other?.msg_info?.msg_output;
+        if (msgInput) {
+          expandDataLocal.push({
+            key: t('用户输入'),
+            value: msgInput,
+          });
+        }
+        if (msgOutput) {
+          expandDataLocal.push({
+            key: t('模型输出'),
+            value: msgOutput,
+          });
+        }
         if (other?.reasoning_effort) {
           expandDataLocal.push({
             key: t('Reasoning Effort'),

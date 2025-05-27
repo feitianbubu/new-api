803a6b9d733d1c586882dd99dae6603ffa23570a
skynono <6811626@qq.com>
2025-05-27 14:43:36 +0800

    feat: web file no cache if DebugEnabled

--- common/embed-file-system.go
+++ common/embed-file-system.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gin-contrib/static"
 	"io/fs"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 // Credit: https://github.com/gin-contrib/static/issues/19
@@ -21,7 +23,27 @@ func (e embedFileSystem) Exists(prefix string, path string) bool {
 	return true
 }
 
+type devFileSystem struct {
+	http.FileSystem
+	root string
+}
+
+func (d devFileSystem) Exists(_ string, path string) bool {
+	fullPath := filepath.Join(d.root, path)
+	_, err := os.Stat(fullPath)
+	return err == nil
+}
+
 func EmbedFolder(fsEmbed embed.FS, targetPath string) static.ServeFileSystem {
+	if DebugEnabled {
+		if _, err := os.Stat(targetPath); err == nil {
+			return devFileSystem{
+				FileSystem: http.Dir(targetPath),
+				root:       targetPath,
+			}
+		}
+	}
+
 	efs, err := fs.Sub(fsEmbed, targetPath)
 	if err != nil {
 		panic(err)

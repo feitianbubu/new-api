284e248eb5436a660f6e39d1db872c2bcf9a0777
skynono <feitianbubu@qq.com>
2025-06-15 12:45:10 +0800

    fix: prevent redirect loop by FRONTEND_BASE_URL configuration

--- router/main.go
+++ router/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"net/url"
 	"one-api/common"
 	"os"
 	"strings"
@@ -24,6 +25,16 @@ func SetRouter(router *gin.Engine, buildFS embed.FS, indexPage []byte) {
 	} else {
 		frontendBaseUrl = strings.TrimSuffix(frontendBaseUrl, "/")
 		router.NoRoute(func(c *gin.Context) {
+			if frontendURL, err := url.Parse(frontendBaseUrl); err == nil {
+				if frontendURL.Host == c.Request.Host {
+					common.LogWarn(c, "Misconfiguration detected: FRONTEND_BASE_URL is set to the address of the slave node itself, which would cause a redirect loop. Please set FRONTEND_BASE_URL to the address of the master node.")
+					c.JSON(http.StatusInternalServerError, gin.H{
+						"success": false,
+						"message": "The service is temporarily unavailable due to a configuration issue. Please contact the administrator.",
+					})
+					return
+				}
+			}
 			c.Redirect(http.StatusMovedPermanently, fmt.Sprintf("%s%s", frontendBaseUrl, c.Request.RequestURI))
 		})
 	}

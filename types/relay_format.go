1a5732cc0c244882e49146932a401038efb773e9
feitianbubu <feitianbubu@qq.com>
2026-03-17 17:02:20 +0800

    feat: add relay format types

--- types/relay_format.go
+++ types/relay_format.go
@@ -11,6 +11,7 @@ const (
 	RelayFormatOpenAIAudio                           = "openai_audio"
 	RelayFormatOpenAIImage                           = "openai_image"
 	RelayFormatOpenAIRealtime                        = "openai_realtime"
+	RelayFormatInteractions                          = "interactions"
 	RelayFormatRerank                                = "rerank"
 	RelayFormatEmbedding                             = "embedding"
 

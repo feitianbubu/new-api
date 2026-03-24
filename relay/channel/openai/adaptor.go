ec959f2ee7a6c6641f437f5e8aaa556f32e4df0c
feitianbubu <feitianbubu@qq.com>
2026-03-24 11:44:06 +0800

    feat: support openai image[] for more image

--- relay/channel/openai/adaptor.go
+++ relay/channel/openai/adaptor.go
@@ -327,6 +327,7 @@ func (a *Adaptor) ConvertOpenAIRequest(c *gin.Context, info *relaycommon.RelayIn
 		if isGPT5Model {
 			request.Temperature = nil
 			request.TopP = nil
+			request.THINKING = nil
 			request.LogProbs = nil
 		}
 

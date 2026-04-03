61df9d42cfdccedbabc980e801183de776490cb2
feitianbubu <feitianbubu@qq.com>
2026-04-04 00:07:27 +0800

    feat: ali fetch task use api key

--- relay/channel/task/ali/constants.go
+++ relay/channel/task/ali/constants.go
@@ -1,6 +1,10 @@
 package ali
 
 var ModelList = []string{
+	"wan2.7-t2v",         // 万相2.7 文生视频（有声）推荐
+	"wan2.7-i2v",         // 万相2.7 图生视频（有声）推荐
+	"wan2.7-r2v",         // 万相2.7 参考生视频
+	"wan2.7-videoedit",   // 万相2.7 视频编辑
 	"wan2.5-i2v-preview", // 万相2.5 preview（有声视频）推荐
 	"wan2.2-i2v-flash",   // 万相2.2极速版（无声视频）
 	"wan2.2-i2v-plus",    // 万相2.2专业版（无声视频）

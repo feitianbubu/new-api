fce668d2c447b4c4e754a3f47a17a0e6a2df0a03
feitianbubu <feitianbubu@qq.com>
2026-04-15 15:46:45 +0800

    feat: add doubao asr 2.0 and multimodal embedding

--- relay/channel/volcengine/constants.go
+++ relay/channel/volcengine/constants.go
@@ -8,12 +8,15 @@ var ModelList = []string{
 	"Doubao-lite-32k",
 	"Doubao-lite-4k",
 	"Doubao-embedding",
+	"doubao-seedream-5-0-260128",
+	"doubao-seedream-4-5-251128",
 	"doubao-seedream-4-0-250828",
 	"seedream-4-0-250828",
 	"doubao-seedance-1-0-pro-250528",
 	"seedance-1-0-pro-250528",
 	"doubao-seed-1-6-thinking-250715",
 	"seed-1-6-thinking-250715",
+	"volc.seedasr.auc",
 }
 
 var ChannelName = "volcengine"

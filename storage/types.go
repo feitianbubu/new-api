537b564b914126bc8be710cf38e422340ef7902b
feitianbubu <feitianbubu@qq.com>
2026-04-21 11:42:27 +0800

    feat: add storage abstraction with tos backend

---
+++ storage/types.go
@@ -0,0 +1,10 @@
+package storage
+
+import "github.com/QuantumNous/new-api/storage/common"
+
+type ExpiresAfter = common.ExpiresAfter
+type FileObject = common.FileObject
+type FileListResult = common.FileListResult
+type UploadOptions = common.UploadOptions
+type ListOptions = common.ListOptions
+type FileContent = common.FileContent

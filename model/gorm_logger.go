dbb91cc96b9bd357dbc869fc2ab5cc0551b203ac
feitianbubu <feitianbubu@qq.com>
2026-07-27 21:13:36 +0800

    fix: validate SQL_SLOW_THRESHOLD_MS range

--- model/gorm_logger.go
+++ model/gorm_logger.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -8,6 +9,11 @@ import (
 	"github.com/QuantumNous/new-api/common"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+)
+
+const (
+	defaultSlowThresholdMs = 200
+	maxSlowThresholdMs     = 60 * 60 * 1000
 )
 
 func newGormConfig(prepareStmt bool) *gorm.Config {
@@ -18,7 +24,11 @@ func newGormConfig(prepareStmt bool) *gorm.Config {
 }
 
 func newGormLogger() logger.Interface {
-	slowThresholdMs := common.GetEnvOrDefault("SQL_SLOW_THRESHOLD_MS", 200)
+	slowThresholdMs := common.GetEnvOrDefault("SQL_SLOW_THRESHOLD_MS", defaultSlowThresholdMs)
+	if slowThresholdMs < 0 || slowThresholdMs > maxSlowThresholdMs {
+		common.SysError(fmt.Sprintf("invalid SQL_SLOW_THRESHOLD_MS %d (allowed 0-%d, 0 disables slow query log), using default %d", slowThresholdMs, maxSlowThresholdMs, defaultSlowThresholdMs))
+		slowThresholdMs = defaultSlowThresholdMs
+	}
 	return logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
 		SlowThreshold:             time.Duration(slowThresholdMs) * time.Millisecond,
 		LogLevel:                  logger.Warn,

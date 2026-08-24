f5ebcdefa75771386f8bfac9dc6c51b5c506aa65
feitianbubu <feitianbubu@qq.com>
2026-08-24 10:50:33 +0800

    feat: return created token info in AddToken response

--- controller/token.go
+++ controller/token.go
@@ -334,10 +334,7 @@ func AddToken(c *gin.Context) {
 		common.ApiError(c, err)
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"success": true,
-		"message": "",
-	})
+	common.ApiSuccess(c, buildMaskedTokenResponse(&cleanToken))
 }
 
 func DeleteToken(c *gin.Context) {
